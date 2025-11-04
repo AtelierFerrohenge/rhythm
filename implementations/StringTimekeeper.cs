1e88d19e274a28f010709e9b26665ccca189fc81
Atelier Ferrohenge <kai@ferrohenge.com>
2025-11-04 01:22:26 -0800

    Basic random target generation for testing

--- implementations/StringTimekeeper.cs
+++ implementations/StringTimekeeper.cs
@@ -7,6 +7,8 @@ public partial class StringTimekeeper : Timekeeper<string>
 	// Input handling and hardcoded inputs are for testing purposes only, final implementation should be more flexible
 	private InputEvent _lastInput;
 
+	private Godot.Collections.Array<string> _targets = ["W", "A", "S", "D"];
+
 	public override void _Input(InputEvent @event)
 	{
 		if (@event.IsActionPressed("W") || @event.IsActionPressed("A") || @event.IsActionPressed("S") || @event.IsActionPressed("D"))
@@ -17,18 +19,19 @@ public partial class StringTimekeeper : Timekeeper<string>
 
 	protected override void UpdateTarget()
 	{
-		Target = "W";
+		Target = _targets.PickRandom();
+		GD.Print("Press " + Target + "!");
 	}
 
 	protected override void CompareTarget()
 	{
 		if (_lastInput?.IsActionPressed(Target) ?? false)
 		{
-			GD.Print("W was last pressed.");
+			GD.Print(Target + " was pressed.");
 		}
 		else
 		{
-			GD.Print("W was not last pressed.");
+			GD.Print(Target + " was not pressed.");
 		}
 	}
 }
